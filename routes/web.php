4a9a06e242e5bade4004af0c04fd4a15e978fbae
Victor Kivungi <kivungijr@gmail.com>
2026-05-23 19:20:45 +0300

    Google Registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\Customer\OrderController as CustomerOrderController;
 use App\Http\Controllers\Customer\ProductController;
