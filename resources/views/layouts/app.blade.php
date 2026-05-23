63a9cf4830a6c6494063efaff2796800ac10e96f
Victor Kivungi <kivungijr@gmail.com>
2026-05-23 18:28:36 +0300

    Vite Disable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 
         <!-- Custom CSS -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 
         <style>
             :root {
